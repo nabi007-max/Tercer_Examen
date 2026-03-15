a1a9fa362d8119ea408a58dd26023ac5fd807f6b
ripolnick8281 <ripolnick8281@gmail.com>
2026-03-14 22:40:17 -0400

    Integrando el Dashboard con IA

--- app/admin.py
+++ app/admin.py
@@ -193,4 +193,5 @@ def configuracion_admin():
     admin.add_view(VentaAdminView(Venta, db.session))
     admin.add_view(DetalleVentaAdminView(DetalleVenta, db.session))
 
+    admin.add_link(MenuLink(name="Dashboard IA", url="/dashboard"))
     admin.add_link(MenuLink(name="Cerrar sesión", url="/logout"))
